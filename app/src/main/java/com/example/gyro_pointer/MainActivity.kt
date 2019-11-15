a74b4c96a1cab9872625337d6d88a32244b615c8
Tonto <rasanko@gmail.com>
2019-11-15 21:39:54 +0100

    implemented movement

--- app/src/main/java/com/example/gyro_pointer/MainActivity.kt
+++ app/src/main/java/com/example/gyro_pointer/MainActivity.kt
@@ -1,27 +1,58 @@
 package com.example.gyro_pointer
 
 import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.Toast
+import android.os.IBinder
+import android.content.ComponentName
+import android.content.Context
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import android.util.Log
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        bindService(Intent(this, MyAccessibilityService::class.java), mConnection, Context.BIND_AUTO_CREATE)
         // check for service permission
         button_service_start.setOnClickListener {
-            var intent = Intent(android.provider.Settings.ACTION_ACCESSIBILITY_SETTINGS)
-            startActivityForResult(intent, 0)
+//            var intent = Intent(android.provider.Settings.ACTION_ACCESSIBILITY_SETTINGS)
+//            startActivityForResult(intent, 0)
 
+
+
+            var intent = Intent(this, MyAccessibilityService::class.java)
+            intent.putExtra("data", "dataaaa")
+            startService(intent)
+
+            
 
         /*  // vjerovatno ga ne treba startat?
             //intent za startat servis?
             var intent = Intent(this, MyAccessibilityService::class.java)
             startService(intent)
         */
+
+        }
+
+
+
+
+    }
+    val mConnection = object : ServiceConnection {
+        override fun onServiceDisconnected(name: ComponentName) {
+            Log.i("MainActivity", "Service Disconncected")
+        }
+
+        override fun onServiceConnected(name: ComponentName, service: IBinder) {
+            Log.i("MainActivity", "Service Conncected")
         }
     }
 }
--- app/src/main/java/com/example/gyro_pointer/MyAccessibilityService.kt
+++ app/src/main/java/com/example/gyro_pointer/MyAccessibilityService.kt
@@ -1,26 +1,42 @@
 package com.example.gyro_pointer
 
 import android.accessibilityservice.AccessibilityService
+import android.accessibilityservice.GestureDescription
 import android.content.Context
 import android.content.res.Resources
+import android.graphics.Path
 import android.graphics.PixelFormat
+import android.os.Binder
+import android.os.IBinder
 import android.util.Log
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.WindowManager
 import android.view.accessibility.AccessibilityEvent
 import android.widget.FrameLayout
-import android.accessibilityservice.GestureDescription
-import android.graphics.Path
 import android.widget.ImageButton
+import java.util.*
+import android.content.Intent
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import androidx.core.app.NotificationCompat.getExtras
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+
+
+
+
 
 
 private lateinit var mLayout: FrameLayout
 private lateinit var windowManager: WindowManager
+private lateinit var layoutParams: WindowManager.LayoutParams
+
 private val screenHeight: Int = Resources.getSystem().displayMetrics.heightPixels
 private val screenWidth: Int = Resources.getSystem().displayMetrics.widthPixels
 
 class MyAccessibilityService : AccessibilityService() {
+
 
     override fun onInterrupt() {
         Log.i("MainService", "onInterrupt() called")
@@ -36,20 +52,8 @@ class MyAccessibilityService : AccessibilityService() {
         Log.i("MainService", "H: " + screenHeight + " W: " + screenWidth)
 
 //        val windowManager = getSystemService(Context.WINDOW_SERVICE) as WindowManager
-        windowManager = getSystemService(Context.WINDOW_SERVICE) as WindowManager
-        mLayout = FrameLayout(this)
-        val layoutParams = WindowManager.LayoutParams()
-        layoutParams.type = WindowManager.LayoutParams.TYPE_ACCESSIBILITY_OVERLAY
-        layoutParams.format = PixelFormat.TRANSLUCENT
-        layoutParams.flags = layoutParams.flags or WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
-        layoutParams.width = WindowManager.LayoutParams.WRAP_CONTENT
-        layoutParams.height = WindowManager.LayoutParams.WRAP_CONTENT
-        layoutParams.gravity = Gravity.START
-        layoutParams.x = (screenWidth/2)
-        layoutParams.y = 0
-        val inflater = LayoutInflater.from(this)
-        inflater.inflate(R.layout.action, mLayout)
-        windowManager.addView(mLayout, layoutParams)
+
+        initPointer()
 
         val callback = object : AccessibilityService.GestureResultCallback() {
             override fun onCompleted(gestureDescription: GestureDescription) {
@@ -66,17 +70,30 @@ class MyAccessibilityService : AccessibilityService() {
         val btn: ImageButton = mLayout.findViewById(R.id.action_button)
         btn.setOnClickListener{
             Log.i("MainService", " kliko san botun")
-            val res: Boolean = dispatchGesture(createClick(5f,5f), callback, null)
-            Log.i("MainService", "result:   " + res.toString())
+//            val res: Boolean = dispatchGesture(createClick(5f,5f), callback, null) //bitno
+//            Log.i("MainService", "result:   " + res.toString())
+
+            movePointer()
+
         }
         //windowManager.removeView(mLayout)
     }
+
+
 
     override fun onAccessibilityEvent(p0: AccessibilityEvent?) {
         Log.i("MainService", "onAccessibilityEvent() called")
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        super.onStartCommand(intent, flags, startId)
+        var data = ""
+        if (intent?.getExtras()?.containsKey("data")!!)
+            data = intent.getStringExtra("data")
+            Log.i("MainService", data)
+        return START_STICKY
+    }
 
     private fun createClick(x: Float, y: Float): GestureDescription {
         // for a single tap a duration of 1 ms is enough
@@ -90,4 +107,39 @@ class MyAccessibilityService : AccessibilityService() {
         return clickBuilder.build()
     }
 
+    fun initPointer(){
+        windowManager = getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        mLayout = FrameLayout(this)
+        layoutParams = WindowManager.LayoutParams()
+        layoutParams.type = WindowManager.LayoutParams.TYPE_ACCESSIBILITY_OVERLAY
+        layoutParams.format = PixelFormat.TRANSLUCENT
+        layoutParams.flags = layoutParams.flags or WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+        layoutParams.width = WindowManager.LayoutParams.WRAP_CONTENT
+        layoutParams.height = WindowManager.LayoutParams.WRAP_CONTENT
+        layoutParams.gravity = Gravity.START
+        layoutParams.x = (screenWidth/2)
+        layoutParams.y = 0
+
+        val inflater = LayoutInflater.from(this)
+        inflater.inflate(R.layout.action, mLayout)
+        windowManager.addView(mLayout, layoutParams)
+
+
+    }
+
+    fun movePointer(){
+
+        val x = Random().nextInt(screenWidth)
+        val y = Random().nextInt(screenHeight) - screenHeight/2
+
+        layoutParams.x = x
+        layoutParams.y = y
+
+        windowManager.updateViewLayout(mLayout, layoutParams)
+
+
+    }
+
+
 }
+
